bb2bdbf311b716631ff5fd4332f5d0fab0f67617
attila <atti80@gmail.com>
2025-08-12 15:54:54 +0200

    fix status bar visibility

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
     return (
         <ClerkProvider tokenCache={tokenCache}>
             <Slot />
+            <StatusBar style="auto" />
         </ClerkProvider>
     )
 }
